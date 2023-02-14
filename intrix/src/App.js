7384e8fb2750a73b31ad02bcf99e4c3a9e111f31
Vaibhav_Thakkar <vaibhavthakkar26051999@gmail.com>
2023-02-14 20:07:31 +0530

    [Add] right side

--- intrix/src/App.js
+++ intrix/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SideBar from './components/SideBar';
+import Main from './components/Main';
 
 function App() {
   return (
-    <>
+    <div class="dashboard">
     <SideBar/>
-    </>
+    <Main/>
+    </div>
   );
 }
 
